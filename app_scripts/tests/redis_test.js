16f71c245dd738b0e43a370871988b5b3c227f23
Nolan Stutelberg <66582795+nstutelberg@users.noreply.github.com>
2023-05-05 00:44:51 -0500

    Update redis_test.js

--- app_scripts/tests/redis_test.js
+++ app_scripts/tests/redis_test.js
@@ -1,17 +1,12 @@
-//const client = createClient({
-//   password: '0CJN62CRMoKeCVDMuvf46J8mSJNtOd09',
-//   host: 'redis-16922.c81.us-east-1-2.ec2.cloud.redislabs.com',
-//   port: 16922
-//});
 
 //const redis = require('redis');
 
 const redis = require("redis");
 
 const client = redis.createClient({
-    password: '0CJN62CRMoKeCVDMuvf46J8mSJNtOd09',
-    host: 'redis-16922.c81.us-east-1-2.ec2.cloud.redislabs.com',
-    port: 16922
+    password: 'SENSITIVE INFORMATION - N/A'',
+    host: 'SENSITIVE INFORMATION - N/A',
+    port: 'SENSITIVE INFORMATION - N/A'
 });
 
 client.on('connect', () => {
@@ -22,11 +17,3 @@ client.on('connect', () => {
 client.on('error', (err) => {
     console.log('Redis client error:', err);
 });
-
-
-// to test: rdcli -h redis-16922.c81.us-east-1-2.ec2.cloud.redislabs.com -a 0CJN62CRMoKeCVDMuvf46J8mSJNtOd09 -p 16922
-//redis-cli -h redis-16922.c81.us-east-1-2.ec2.cloud.redislabs.com -a 0CJN62CRMoKeCVDMuvf46J8mSJNtOd09
-//redis-cli -h redis-16922.c81.us-east-1-2.ec2.cloud.redislabs.com -p 16922 -a
-// can do PING, or SET key1 value1, then GET key1
-
-//it works on ubuntu when I log into the server with redis-cli, and this code uses the same info but doesn't return results
